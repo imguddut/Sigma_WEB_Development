686364c14891eab15300e720836b15515e4b3b4a
NAVAL KISHOR KUMAR <navalkishkre@gmail.com>
2024-02-04 12:27:17 +0530

    Day-14

---
+++ Day-14/script.js
@@ -0,0 +1,18 @@
+alert("Hello world");
+
+console.log("code is running...")
+console.log("code is also running...")
+console.log("code is running...")
+
+var a = prompt("Enter your number ")
+var isTrue = confirm("Are you sure want to blast ur computer ")
+
+if(isTrue){
+    console.log("Computer is balsting ")
+}
+
+else{
+    console.log("Computer is not blasting")
+}
+
+console.log("Your number is " +a)
